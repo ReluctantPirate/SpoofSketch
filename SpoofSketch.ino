57c86e25566effe348af0beb7ba3128e13739f52
ReluctantPirate <reluctantpirategames@gmail.com>
2020-08-27 12:50:18 -0400

    updated graphics, new quick reset function

--- SpoofSketch.ino
+++ SpoofSketch.ino
@@ -1,16 +1,14 @@
-enum signalStates {SETUP, HIDE, REVEAL};
+enum signalStates {SETUP, HIDE, DEATH, RESET};
 byte signalState = SETUP;
 
-bool isOn = false;
-
+bool isMagma = false;
 bool isChaos = false;
-
-#define PREVIEW_TIME 1000
-Timer previewTimer;
+bool isRevealed = false;
+bool isKiller = false;
 
 void setup() {
   // put your setup code here, to run once:
-
+  randomize();
 }
 
 void loop() {
@@ -23,9 +21,13 @@ void loop() {
       hideLoop();
       hideDisplay();
       break;
-    case REVEAL:
-      revealLoop();
-      revealDisplay();
+    case DEATH:
+      deathLoop();
+      deathDisplay();
+      break;
+    case RESET:
+      resetLoop();
+      setupDisplay();
       break;
   }
 
@@ -38,18 +40,21 @@ void loop() {
 
 void setupLoop() {
 
-  if (buttonSingleClicked() && !isChaos) {
-    isOn = !isOn;
+  if (buttonSingleClicked()) {
+    isMagma = !isMagma;
   }
 
   if (buttonDoubleClicked()) {
     signalState = HIDE;
+    setGrass();
     if (isChaos) {
-      isOn = random(1);
+      isMagma = random(1);
     }
+
+
   }
 
-  if (buttonMultiClicked() && buttonClickCount() == 3) {
+  if (buttonMultiClicked() && buttonClickCount() == 4) {
     isChaos = !isChaos;
   }
 
@@ -57,8 +62,9 @@ void setupLoop() {
     if (!isValueReceivedOnFaceExpired(f)) {//neighbor!
       if (getLastValueReceivedOnFace(f) == HIDE) {
         signalState = HIDE;
+        setGrass();
         if (isChaos) {
-          isOn = random(1);
+          isMagma = random(1);
         }
       }
     }
@@ -68,27 +74,33 @@ void setupLoop() {
 void hideLoop() {
 
   if (buttonDoubleClicked()) {
-    signalState = REVEAL;
+    signalState = RESET;
   }
 
   FOREACH_FACE(f) {
     if (!isValueReceivedOnFaceExpired(f)) {//neighbor!
-      if (getLastValueReceivedOnFace(f) == REVEAL) {
-        signalState = REVEAL;
+      if (getLastValueReceivedOnFace(f) == DEATH) {
+        signalState = DEATH;
+      } else if (getLastValueReceivedOnFace(f) == RESET) {
+        signalState = RESET;
       }
     }
   }
 
   if (buttonSingleClicked()) {
-    previewTimer.set(PREVIEW_TIME);
+    isRevealed = true;
+    if (isMagma) {
+      signalState = DEATH;
+      isKiller = true;
+    }
   }
 }
 
-void revealLoop() {
+void deathLoop() {
   if (buttonDoubleClicked()) {
     signalState = SETUP;
     if (isChaos) {
-      isOn = false;
+      isMagma = false;
     }
   }
 
@@ -96,70 +108,92 @@ void revealLoop() {
     if (!isValueReceivedOnFaceExpired(f)) {//neighbor!
       if (getLastValueReceivedOnFace(f) == SETUP) {
         signalState = SETUP;
-        if (isChaos) {
-          isOn = false;
-        }
+      } else if (getLastValueReceivedOnFace(f) == RESET) {
+        signalState = RESET;
+      }
+    }
+  }
+}
+
+void resetLoop() {
+  signalState = SETUP;
+
+  FOREACH_FACE(f) {
+    if (!isValueReceivedOnFaceExpired(f)) {//neighbor!
+      if (getLastValueReceivedOnFace(f) == HIDE || getLastValueReceivedOnFace(f) == DEATH) {
+        signalState = RESET;
       }
     }
   }
 }
 
 void setupDisplay() {
-  //set base colors
-  setColor(YELLOW);
-  setColorOnFace(dim(YELLOW, 150), 2);
-  setColorOnFace(dim(YELLOW, 150), 3);
-  setColorOnFace(dim(YELLOW, 100), 4);
-  setColorOnFace(dim(YELLOW, 100), 5);
+  byte magmaBrightness;
+  byte grassBrightness;
 
-  if (isOn) {
-    byte segmentLight = (millis() / 200) % 4;
-    switch (segmentLight) {
-      case 0:
-        setColorOnFace(RED, 0);
-        setColorOnFace(RED, 1);
-        break;
-      case 1:
-        setColorOnFace(dim(RED, 150), 2);
-        setColorOnFace(dim(RED, 150), 3);
-        break;
-      case 2:
-        setColorOnFace(dim(RED, 100), 4);
-        setColorOnFace(dim(RED, 100), 5);
-        break;
-    }
+  if (isMagma) {
+    magmaBrightness = 255;
+    grassBrightness = 100;
+  } else {
+    magmaBrightness = 100;
+    grassBrightness = 255;
   }
 
   if (isChaos) {
-    setColorOnFace(OFF, random(5));
+    setColorOnFace(dim(RED, random(155) + 100), 0);
+    setColorOnFace(dim(RED, random(155) + 100), 2);
+    setColorOnFace(dim(RED, random(155) + 100), 4);
+    setColorOnFace(dim(GREEN, random(155) + 100), 1);
+    setColorOnFace(dim(GREEN, random(155) + 100), 3);
+    setColorOnFace(dim(GREEN, random(155) + 100), 5);
+  } else {
+    setColorOnFace(dim(RED, magmaBrightness), 0);
+    setColorOnFace(dim(RED, magmaBrightness), 2);
+    setColorOnFace(dim(RED, magmaBrightness), 4);
+    setColorOnFace(dim(GREEN, grassBrightness), 1);
+    setColorOnFace(dim(GREEN, grassBrightness), 3);
+    setColorOnFace(dim(GREEN, grassBrightness), 5);
+  }
+}
+
+#define GRASS_HUE_LO 60
+#define GRASS_HUE_HI 100
+byte grassHues[6];
+
+void setGrass() {
+  isRevealed = false;
+  isKiller = false;
+  FOREACH_FACE(f) {
+    grassHues[f] = GRASS_HUE_LO + random(GRASS_HUE_HI - GRASS_HUE_LO);
   }
 }
 
 void hideDisplay() {
-  setColor(OFF);
-  byte segmentLight = (millis() / 1000) % 3;
-  byte segmentProgress = map(millis() % 1000, 0, 1000, 0, 255);
-  setColorOnFace(dim(YELLOW, segmentProgress), segmentLight * 2);
-  setColorOnFace(dim(YELLOW, segmentProgress), (segmentLight * 2) + 1);
+  //determine brightness
+  byte grassBrightness = 255;
+  if (isRevealed) {
+    grassBrightness = 100;
+  }
 
-  if (!previewTimer.isExpired()) {
-    if (isOn) {
-      setColorOnFace(RED, random(5));
-      setColorOnFace(RED, random(5));
-    } else {
-      setColorOnFace(CYAN, random(5));
-      setColorOnFace(CYAN, random(5));
-    }
+  FOREACH_FACE(f) {
+    setColorOnFace(makeColorHSB(grassHues[f], 255, grassBrightness), f);
   }
 }
 
-void revealDisplay() {
-  setColor(OFF);
-  if (isOn) {
-    setColorOnFace(RED, random(5));
-    setColorOnFace(RED, random(5));
+void deathDisplay() {
+
+  if (!isKiller) {
+    byte fireFace = random(5);
+    FOREACH_FACE(f) {
+      if (f == fireFace) {
+        setColorOnFace(RED, f);
+      } else {
+        setColorOnFace(makeColorHSB(grassHues[f], 255, 150), f);
+      }
+    }
   } else {
-    setColorOnFace(CYAN, random(5));
-    setColorOnFace(CYAN, random(5));
+    setColor(RED);
+    setColorOnFace(ORANGE, random(5));
+    setColorOnFace(ORANGE, random(5));
   }
 }
